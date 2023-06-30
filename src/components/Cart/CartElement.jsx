dd334fab3a996ba97f6955a53de7ec00ca594695
Brandon <name.brandon.aguero@gmail.com>
2023-06-30 08:57:29 -0500

    La ruta cart es responsive para dispositivos pequeños

--- src/components/Cart/CartElement.jsx
+++ src/components/Cart/CartElement.jsx
@@ -9,22 +9,31 @@ const CartElement = ({ product }) => {
   };
 
   return (
-    <article>
-      <header>
-        <img src={product.product.images[0].url} />
+    <article className="grid grid-cols-4 grid-rows-2 gap-x-2">
+      <header className="col-start-1 col-end-2 row-start-1 row-end-2">
+        <img
+          className="h-20 w-20 object-contain"
+          src={product.product.images[0].url}
+        />
       </header>
-      <section>
-        <h3>{product.product.title}</h3>
+      <section className="col-start-2 col-end-4 row-start-1 row-end-2">
+        <h3 className="font-semibold text-stone-600">
+          {product.product.title}
+        </h3>
         <p>
           <span>{product.quantity}</span> x <span>{product.product.price}</span>
         </p>
-        <button onClick={handleDelete}>
+      </section>
+      <section className="text-center">
+        <button className="text-red-500" onClick={handleDelete}>
           <i className="bx bx-trash"></i>
         </button>
       </section>
-      <footer>
-        <span>Subtotal</span>
-        <span>{product.quantity * product.product.price}</span>
+      <footer className="col-start-3 col-end-5 row-start-2 row-end-2 flex gap-2 self-center justify-self-end text-xs">
+        <span className="text-stone-600">Total</span>
+        <span className="font-semibold text-stone-800">
+          $ {product.quantity * product.product.price}
+        </span>
       </footer>
     </article>
   );
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -17,19 +17,24 @@ const CartPage = () => {
   };
 
   return (
-    <div>
-      <h2>Cart</h2>
-      <div>
+    <div className="relative top-16 flex h-screen w-screen flex-col items-center gap-2">
+      <h2 className="w-4/5 text-xl font-bold text-stone-700">Shopping Cart</h2>
+      <div className="flex h-[60%] w-4/5 flex-col gap-2 overflow-y-scroll">
         {cart?.map((product) => (
           <CartElement key={product.id} product={product} />
         ))}
       </div>
-      <footer>
-        <div>
-          <span>Total</span>
-          <span>{totalPrice}</span>
+      <footer className="z-20 flex h-[20%] w-4/5 flex-col items-center justify-center gap-2 bg-white font-serif">
+        <div className="flex w-full justify-between font-semibold">
+          <span className="text-stone-600">Total</span>
+          <span className="text-stone-800">$ {totalPrice}</span>
         </div>
-        <button onClick={handlePurchase}>Purchase this cart</button>
+        <button
+          className="w-48 bg-red-500 p-1 text-white hover:brightness-110"
+          onClick={handlePurchase}
+        >
+          Checkout
+        </button>
       </footer>
     </div>
   );
